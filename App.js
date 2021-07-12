d33af120bb1a1c0b8b8030affc848860d3837412
sentinelbot <ikmazameti@gmail.com>
2021-07-12 10:16:38 +0000

    Todo Item

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.todoContainer}>
+        <FontAwesome name="circle-thin" size={24} color="black" />
+        <Text style={styles.todoText}>Learn Kotlin!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  todoContainer: {
+    backgroundColor: "#021",
+    width: "100%",
+    flexDirection: "row",
+    padding: 10,
+  },
+  todoText: {
+    alignItems: "center",
+    fontSize: 14,
+    color: "#000",
+    paddingStart: 20,
   },
 });
